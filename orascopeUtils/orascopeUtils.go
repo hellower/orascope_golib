778e85ec39cc01c691730a14c868e1ceb1c13f84
Your Name <you@example.com>
2022-07-19 11:07:14 +0900

    fdas

--- orascopeUtils/orascopeUtils.go
+++ orascopeUtils/orascopeUtils.go
@@ -8,7 +8,7 @@ import (
 // ByteSliceToString is used when you really want to convert a slice // of bytes to a string without incurring overhead. It is only safe
 // to use if you really know the byte slice is not going to change // in the lifetime of the string
 //https://go.dev/src/strings/builder.go#L45
-func ByteliceToString(bs []byte) string {
+func ByteSliceToString(bs []byte) string {
 	// This is copied from runtime. It relies on the string
 	// header being a prefix of the slice header!
 	return *(*string)(unsafe.Pointer(&bs))
