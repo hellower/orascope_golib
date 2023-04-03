c2e2cd915546512c07565a500c65b802bfa12bdc
Your Name <you@example.com>
2023-04-03 09:10:20 +0900

    fdsa

--- orascopeUtils/go.mod
+++ orascopeUtils/go.mod
@@ -1,3 +1,3 @@
 module github.com/hellower/orascope_golib/orascopeUtils
 
-go 1.18
+go 1.20
--- orascopeUtils/orascopeUtils.go
+++ orascopeUtils/orascopeUtils.go
@@ -1,13 +1,14 @@
 package orascopeUtils
 
 import (
-	"reflect"
+	//	"reflect"
 	"unsafe"
 )
 
+/****************************************************************************************************************************************
 // ByteSliceToString is used when you really want to convert a slice // of bytes to a string without incurring overhead. It is only safe
 // to use if you really know the byte slice is not going to change // in the lifetime of the string
-//https://go.dev/src/strings/builder.go#L45
+// https://go.dev/src/strings/builder.go#L45
 func ByteSliceToString(bs []byte) string {
 	// This is copied from runtime. It relies on the string
 	// header being a prefix of the slice header!
@@ -19,4 +20,14 @@ func StringToByteSlice(s string) []byte {
 	const MaxInt32 = 1<<31 - 1
 	return (*[MaxInt32]byte)(unsafe.Pointer((*reflect.StringHeader)(
 		unsafe.Pointer(&s)).Data))[: len(s)&MaxInt32 : len(s)&MaxInt32]
+}
+****************************************************************************************************************************************/
+
+// For Go 1.20 and higher
+func StringToByteSlice(s string) []byte {
+	return unsafe.Slice(unsafe.StringData(s), len(s))
+}
+
+func ByteSliceToString(b []byte) string {
+	return unsafe.String(unsafe.SliceData(b), len(b))
 }
